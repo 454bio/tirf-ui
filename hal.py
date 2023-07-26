be92f2a8522968207d628042282c971fdd021e5b
Albert Armea <github@albertarmea.com>
2023-07-25 21:03:55 -0400

    Throw on HAL error

--- hal.py
+++ hal.py
@@ -6,17 +6,21 @@ import socket
 ENCODING = "utf-8"
 MAX_RESPONSE_SIZE = 1 << 10
 
+class HalError(Exception):
+    pass
+
 @dataclass
 class Hal:
     socket_path: str
 
     def run_command(self, command: Dict):
-        print(command) # XXX
         with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as s:
             s.connect(self.socket_path)
 
-            request = json.dumps(command).encode(ENCODING)
-            s.sendall(request)
+            request_raw = json.dumps(command).encode(ENCODING)
+            s.sendall(request_raw)
 
-            response = s.recv(MAX_RESPONSE_SIZE).decode(ENCODING)
-            print(response) # XXX
+            response_raw = s.recv(MAX_RESPONSE_SIZE)
+            response = json.loads(response_raw.decode(ENCODING))
+            if not response["success"]:
+                raise HalError(response["error_message"])

984e64bb3ab3203af9f513e6a01d6b6325065f02
Albert Armea <github@albertarmea.com>
2023-08-13 00:05:55 -0400

    Fix reading preview from socket

--- preview.py
+++ preview.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from typing import List
+import math
 import socket
 import time
 import traceback
@@ -13,7 +14,7 @@ PREVIEW_ROWS = 2
 PREVIEW_COLS = 2
 
 PREVIEW_HEADER_FORMAT = "u4u12u12u6u26"
-PREVIEW_HEADER_SIZE = bitstruct.calcsize(PREVIEW_HEADER_FORMAT)
+PREVIEW_HEADER_SIZE = math.ceil(bitstruct.calcsize(PREVIEW_HEADER_FORMAT) / 8)
 
 class PreviewThread(QThread):
     def __init__(self, socket_path: Path):
@@ -22,38 +23,30 @@ class PreviewThread(QThread):
 
     @Slot(None)
     def run(self):
-        with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as s:
-            while True:
-                try:
+        while True:
+            try:
+                with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as s:
                     s.connect(str(self.socket_path))
                     while True:
                         self.read_preview_bytes(s)
-                except InterruptedError:
-                    break
-                except:
-                    # Something went wrong with the preview socket. Let me know and wait a bit before trying again.
-                    traceback.print_exc()
-                    time.sleep(5)
+            except InterruptedError:
+                break
+            except:
+                # Something went wrong with the preview socket. Let me know and wait a bit before trying again.
+                traceback.print_exc()
+                time.sleep(5)
 
     def read_preview_bytes(self, s: socket.socket) -> bytes:
         header = s.recv(PREVIEW_HEADER_SIZE)
-        print(len(header))
         assert len(header) == PREVIEW_HEADER_SIZE
 
         (version, width, height, imageFormat, image_size) = bitstruct.unpack(PREVIEW_HEADER_FORMAT, header)
-        print(version, width, height, imageFormat, image_size)  # XXX
         assert version == 0
 
-        image_bytes = bytes()
+        image_bytes = bytearray()
         while len(image_bytes) < image_size:
-            image_bytes += s.recv(image_size - len(image_bytes))
-            print(f"Read {len(image_bytes)} of {image_size} bytes")  # XXX
+            image_bytes.extend(s.recv(image_size - len(image_bytes)))
 
-        print("Read entire preview image")  # XXX
-        with open("foo", "wb") as f:
-            f.write(image_bytes)
-        # TODO: The image appears to be shorter than the header declared -- there is a new header slightly too early.
-        # This is throwing off read alignments.
         return image_bytes
 
 class PreviewWidget(QWidget):

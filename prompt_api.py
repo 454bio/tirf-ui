bf0fa3a8e0a2a0ee62cfe8d532cc467332ee039f
Albert Armea <github@albertarmea.com>
2023-09-05 22:02:13 -0400

    WIP: Implement Andor camera control via prompt API
    Andor cameras require a proprietary driver that is only available on x86
    Windows and Linux and therefore can't be used directly on the Pi.
    Instead, the GUI exposes access to the camera over the prompt API. The
    HAL continues to be primarily responsible for running image sequences
    and manages the GPIO.
    
    This change implements the initial setup, image capture, and teardown of
    the camera. The following work is NYI:
    
    * Saving the images
    * Image preview (particularly important because this deprecates usage of
      SOLIS alongside the GUI)

--- prompt_api.py
+++ prompt_api.py
@@ -1,3 +1,4 @@
+import atexit
 import json
 from functools import partial
 
@@ -51,6 +52,48 @@ class PromptApi(QObject):
                 text = request["text"]
                 prompt = ConfirmationPrompt(self.parent(), text)
                 success = bool(prompt.exec_())
+            elif command == "camera_setup_and_start":
+                if self.camera is None:
+                    # This is slow, so only import when needed -- a HAL with its own camera will never call this.
+                    from pylablib.devices import Andor
+                    self.camera = Andor.AndorSDK3Camera()
+                    atexit.register(self.camera.close)
+
+                    # GPIO options.
+                    self.camera.set_trigger_mode("ext")
+                    self.camera.cav["AuxiliaryOutSource"] = "FireAll"
+                    for io_name in self.camera.get_attribute("IOSelector").values:
+                        # Setting IOSelector doesn't do anything by itself.
+                        self.camera.cav["IOSelector"] = io_name
+                        # Instead, it switches what the other IO* attributes are referencing.
+                        # Changing these will only affect the IO at io_name.
+                        self.camera.cav["IOInvert"] = True
+
+                    # Image format settings.
+                    # Available values are "100 MHz" and "270 MHz", which appear to be halved from the "200 MHz" and "540 MHz" options present in the GUI.
+                    self.camera.cav["PixelReadoutRate"] = "100 MHz"
+                    self.camera.cav["BitDepth"] = "16 Bit"
+
+                    # The HAL should call `camera_stop_and_save` at the end of each image sequence.
+                    # "sequence" corresponds to video mode. The camera will still only capture on the external trigger.
+                    # A typical image sequence is only 4 images long, but let's leave lots of room for exceptional cases.
+                    self.camera.setup_acquisition(mode="sequence", nframes=128)
+
+                exposure_ms = int(request["camera_parameters"]["exposure_time_ms"])
+                self.camera.set_exposure(exposure_ms / 1000)
+
+                self.camera.start_acquisition()
+            elif command == "camera_stop_and_save":
+                if self.camera is None or not self.camera.cav["CameraAcquiring"]:
+                    raise Exception("Camera not configured")
+
+                # TODO: Wait until all of the images we care about are available?
+                # Need to guess how long (not great) or have the HAL tell us how many images to expect
+                # It may make more sense to just save after each capture, which will also enable immediate preview
+                images = self.camera.read_multiple_images()
+                # TODO: Save the images
+                # TODO: Preview them?
+                self.camera.stop_acquisition()
             else:
                 raise ValueError(f"Unknown command {command}")
         except Exception as e:
